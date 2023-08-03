8fbbc221bbf0b9d97bcf3bc0a43f31dc6cf7a5c5
Quramy <yosuke.kurami@gmail.com>
2023-08-04 04:50:34 +0900

    chore: Add albums seed

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client'
 import { initialize } from '@quramy/prisma-fabbrica'
-import { ArtistFactory } from '../src/factories'
+import { ArtistFactory, AlbumFactory } from '../src/factories'
 
 export async function main() {
   const prisma = new PrismaClient()
@@ -13,6 +13,13 @@ export async function main() {
     name: 'John Coltrane',
     biography:
       'In 1955, Coltrane was freelancing in Philadelphia while studying with Sandole when he received a call from trumpeter Miles Davis. Davis had been successful in the 1940s, but his reputation and work had been damaged in part by heroin addiction; he was again active and about to form a quintet. Coltrane was with this edition of the Davis band (known as the "First Great Quintet"—along with Red Garland on piano, Paul Chambers on bass, and Philly Joe Jones on drums) from October 1955 to April 1957 (with a few absences).',
+    albums: {
+      create: await AlbumFactory.buildList([
+        { name: 'Giant Steps', releaseYear: '1960' },
+        { name: 'Coltrane Jazz', releaseYear: '1961' },
+        { name: 'My Favorite Things', releaseYear: '1961' },
+      ]),
+    },
   })
 }
 
